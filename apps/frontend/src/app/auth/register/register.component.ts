c601d4b5f24a59f9fc48be155f126d3a0065930e
itapit <Itamardavid90@gmail.com>
2025-09-30 14:20:17 +0300

    setup conf

---
+++ apps/frontend/src/app/auth/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  standalone: false,
+  imports: [],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {}
