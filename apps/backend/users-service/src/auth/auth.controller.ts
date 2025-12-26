8d0d0d6732f0d167a79c4ab006e7627d9c3edeff
Itapit <itamardavid90@gmail.com>
2025-12-26 15:53:03 +0200

    refactor(users-service): update AuthController imports and return types

--- apps/backend/users-service/src/auth/auth.controller.ts
+++ apps/backend/users-service/src/auth/auth.controller.ts
@@ -1,41 +1,43 @@
 import {
   AuthLoginPayload,
+  AuthLoginResponseDto,
   AuthLogoutAllPayload,
   AuthLogoutPayload,
   AuthRefreshPayload,
   CompleteSetupPayload,
+  PendingLoginResponseDto,
   USER_PATTERNS,
 } from '@LucidRF/users-contracts';
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { AuthService } from './application/auth.service';
+import { AuthService } from './application/services/auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @MessagePattern(USER_PATTERNS.AUTH_LOGIN)
-  handleLogin(@Payload() payload: AuthLoginPayload) {
+  handleLogin(@Payload() payload: AuthLoginPayload): Promise<AuthLoginResponseDto | PendingLoginResponseDto> {
     return this.authService.login(payload);
   }
 
   @MessagePattern(USER_PATTERNS.AUTH_COMPLETE_SETUP)
-  handleCompleteSetup(@Payload() payload: CompleteSetupPayload) {
+  handleCompleteSetup(@Payload() payload: CompleteSetupPayload): Promise<AuthLoginResponseDto> {
     return this.authService.completeUserSetup(payload);
   }
 
   @MessagePattern(USER_PATTERNS.AUTH_REFRESH)
-  handleRefresh(@Payload() payload: AuthRefreshPayload) {
+  handleRefresh(@Payload() payload: AuthRefreshPayload): Promise<AuthLoginResponseDto> {
     return this.authService.refreshAccessToken(payload);
   }
 
   @MessagePattern(USER_PATTERNS.AUTH_LOGOUT)
-  handleLogout(@Payload() payload: AuthLogoutPayload) {
+  handleLogout(@Payload() payload: AuthLogoutPayload): Promise<{ success: true }> {
     return this.authService.logout(payload);
   }
 
   @MessagePattern(USER_PATTERNS.AUTH_LOGOUT_ALL)
-  handleLogoutAll(@Payload() payload: AuthLogoutAllPayload) {
+  handleLogoutAll(@Payload() payload: AuthLogoutAllPayload): Promise<{ success: true }> {
     return this.authService.logoutAll(payload);
   }
 }
