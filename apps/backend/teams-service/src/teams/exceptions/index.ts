029cb3cbd75db6127d266c75a0cbeb23e30008cb
Itapit <itamardavid90@gmail.com>
2026-03-08 14:41:49 +0200

    refactor(teams): update exception exports

--- apps/backend/teams-service/src/teams/exceptions/index.ts
+++ apps/backend/teams-service/src/teams/exceptions/index.ts
@@ -1,5 +1,3 @@
-export * from './team-name-exists.exception';
+export * from './invalid-team-operation.exception';
 export * from './team-not-found.exception';
 export * from './team-permission-denied.exception';
-export * from './invalid-team-id.exception';
-export * from './invalid-team-operation.exception';
