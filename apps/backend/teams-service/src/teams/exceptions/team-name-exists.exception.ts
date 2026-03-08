26cacfa2c35c6029a0541835fc17dc1da38cfc1b
Itapit <itamardavid90@gmail.com>
2026-03-08 14:20:11 +0200

    chore(teams): Remove unused team exceptions and error codes

--- apps/backend/teams-service/src/teams/exceptions/team-name-exists.exception.ts
+++
@@ -1,9 +0,0 @@
-import { BaseDomainException } from '@LucidRF/backend-common';
-import { TeamErrorCodes } from '@LucidRF/common';
-import { HttpStatus } from '@nestjs/common';
-
-export class TeamNameExistsException extends BaseDomainException {
-  constructor(name: string) {
-    super(`Team name '${name}' is already taken`, HttpStatus.CONFLICT, TeamErrorCodes.TEAM_NAME_EXISTS);
-  }
-}
