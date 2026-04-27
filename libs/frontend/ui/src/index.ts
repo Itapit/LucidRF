58321364e022400f6b96811b4670998d40f0fd71
Itapit <itamardavid90@gmail.com>
2026-04-27 10:26:56 +0300

    feat(ui): Introduce common UI type enums (ComponentSize, AdminTab, WorkspaceNavigationId)

--- libs/frontend/ui/src/index.ts
+++ libs/frontend/ui/src/index.ts
@@ -2,6 +2,7 @@ export * from './lib/atoms';
 export * from './lib/molecules';
 export * from './lib/organisms';
 export * from './lib/templates';
+export * from './lib/types';
 
 export * from './lib/organisms/admin-users-table/admin-users-table.component';
 export * from './lib/organisms/user-create-modal/user-create-modal.component';
